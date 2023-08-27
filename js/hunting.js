811197587f24661ea796ff468225a2888a483dec
manik712 <ranineela71@gmail.com>
2023-08-27 08:45:34 -0700

    added the main section

--- js/hunting.js
+++ js/hunting.js
@@ -2,7 +2,43 @@ const loadPhone = async () =>{
   const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
   const data = await res.json();
   const phones =data.data
-  console.log(phones);
+  // console.log(phones);
+  displayPlay(phones);
 }
+
+
+
+const displayPlay = phones =>{
+// console.log(phones);
+
+
+const phoneContainer =document.getElementById('phone-container');
+phones.forEach(phone=>{
+  console.log(phone);
+  // 1 create a div 
+  const phoneCard = document.createElement('div');
+  phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+  phoneCard.innerHTML = `
+  
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+  
+  
+  
+  
+  `
+})
+
+}
+
+
+
+
 
 loadPhone();
